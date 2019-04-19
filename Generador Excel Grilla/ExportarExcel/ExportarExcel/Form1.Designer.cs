8d4973bf6ac35a1f69fb0ea328a49293141b881e
Fernando Raihuanque Cid - w10 <fernando.arc02@gmail.com>
2019-04-18 23:05:50 -0400

    respaldo

---
+++ Generador Excel Grilla/ExportarExcel/ExportarExcel/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ExportarExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvRegistros = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAgregarFila = new System.Windows.Forms.Button();
+            this.btnExportarExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvRegistros
+            // 
+            this.dgvRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRegistros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11});
+            this.dgvRegistros.Location = new System.Drawing.Point(12, 139);
+            this.dgvRegistros.Name = "dgvRegistros";
+            this.dgvRegistros.Size = new System.Drawing.Size(1149, 150);
+            this.dgvRegistros.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Column10";
+            this.Column10.Name = "Column10";
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Column11";
+            this.Column11.Name = "Column11";
+            // 
+            // btnAgregarFila
+            // 
+            this.btnAgregarFila.Location = new System.Drawing.Point(104, 72);
+            this.btnAgregarFila.Name = "btnAgregarFila";
+            this.btnAgregarFila.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregarFila.TabIndex = 1;
+            this.btnAgregarFila.Text = "Agregar Registro";
+            this.btnAgregarFila.UseVisualStyleBackColor = true;
+            this.btnAgregarFila.Click += new System.EventHandler(this.BtnAgregarFila_Click);
+            // 
+            // btnExportarExcel
+            // 
+            this.btnExportarExcel.Location = new System.Drawing.Point(1076, 71);
+            this.btnExportarExcel.Name = "btnExportarExcel";
+            this.btnExportarExcel.Size = new System.Drawing.Size(75, 23);
+            this.btnExportarExcel.TabIndex = 2;
+            this.btnExportarExcel.Text = "Exportar Excel";
+            this.btnExportarExcel.UseVisualStyleBackColor = true;
+            this.btnExportarExcel.Click += new System.EventHandler(this.BtnExportarExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 450);
+            this.Controls.Add(this.btnExportarExcel);
+            this.Controls.Add(this.btnAgregarFila);
+            this.Controls.Add(this.dgvRegistros);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvRegistros;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.Button btnAgregarFila;
+        private System.Windows.Forms.Button btnExportarExcel;
+    }
+}
+
---
+++ Generador Excel Grilla/ExportarExcel/ExportarExcel/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace ExportarExcel
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAgregarFila_Click(object sender, EventArgs e)
+        {
+            //aca recibiria los datos provenientes de la compuerta com
+            byte[] fila = { 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41 };
+
+            //crear una fila con los datos y agregarlos al data grid view
+            dgvRegistros.Rows.Add(fila[0], fila[1], fila[2], fila[3], fila[4], fila[5], fila[6], fila[7], fila[8], fila[9], fila[10]);
+
+
+        }
+
+        private void BtnExportarExcel_Click(object sender, EventArgs e)
+        {
+            // es importante definir el nombre de las columnas que se encuentran en el data grid view
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Column1");
+            dt.Columns.Add("Column2");
+            dt.Columns.Add("Column3");
+            dt.Columns.Add("Column4");
+            dt.Columns.Add("Column5");
+            dt.Columns.Add("Column6");
+            dt.Columns.Add("Column7");
+            dt.Columns.Add("Column8");
+            dt.Columns.Add("Column9");
+            dt.Columns.Add("Column10");
+            dt.Columns.Add("Column11");
+
+            foreach (DataGridViewRow rowGrid in dgvRegistros.Rows)
+            {
+                //recorrer todos los registro de la grilla
+                DataRow row = dt.NewRow();
+                row["column1"] = rowGrid.Cells[0].Value;
+                row["column2"] = rowGrid.Cells[1].Value;
+                row["column3"] = rowGrid.Cells[2].Value;
+                row["column4"] = rowGrid.Cells[3].Value;
+                row["column5"] = rowGrid.Cells[4].Value;
+                row["column6"] = rowGrid.Cells[5].Value;
+                row["column7"] = rowGrid.Cells[6].Value;
+                row["column8"] = rowGrid.Cells[7].Value;
+                row["column9"] = rowGrid.Cells[8].Value;
+                row["column10"] = rowGrid.Cells[9].Value;
+                row["column11"] = rowGrid.Cells[10].Value;
+
+                dt.Rows.Add(row);
+            }
+
+            Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+            Excel.Workbook LibroExcel;
+            Excel.Worksheet HojaExcel;
+            object misValue = System.Reflection.Missing.Value;
+
+            LibroExcel = xlApp.Workbooks.Add(misValue);
+            HojaExcel = (Excel.Worksheet)LibroExcel.Worksheets.get_Item(1);
+
+            //crear cabecera excel
+            HojaExcel.Cells[1, 1] = "Column1";
+            HojaExcel.Cells[1, 2] = "Column2";
+            HojaExcel.Cells[1, 3] = "Column3";
+            HojaExcel.Cells[1, 4] = "Column4";
+            HojaExcel.Cells[1, 5] = "Column5";
+            HojaExcel.Cells[1, 6] = "Column6";
+            HojaExcel.Cells[1, 7] = "Column7";
+            HojaExcel.Cells[1, 8] = "Column8";
+            HojaExcel.Cells[1, 9] = "Column9";
+            HojaExcel.Cells[1, 10] = "Column10";
+            HojaExcel.Cells[1, 11] = "Column11";
+
+            //agregar toda la data del data table al archivo excel
+            int aux = 2;
+            foreach (DataRow dataRow in dt.Rows)
+            {
+                HojaExcel.Cells[aux, 1] = dataRow[0].ToString();
+                HojaExcel.Cells[aux, 2] = dataRow[1].ToString();
+                HojaExcel.Cells[aux, 3] = dataRow[2].ToString();
+                HojaExcel.Cells[aux, 4] = dataRow[3].ToString();
+                HojaExcel.Cells[aux, 5] = dataRow[4].ToString();
+                HojaExcel.Cells[aux, 6] = dataRow[5].ToString();
+                HojaExcel.Cells[aux, 7] = dataRow[6].ToString();
+                HojaExcel.Cells[aux, 8] = dataRow[7].ToString();
+                HojaExcel.Cells[aux, 9] = dataRow[8].ToString();
+                HojaExcel.Cells[aux, 10] = dataRow[9].ToString();
+                HojaExcel.Cells[aux, 11] = dataRow[10].ToString();
+                aux++;
+            }
+
+            //cerrar el libro excel
+            LibroExcel.SaveAs("C:\\prueba\\csharp-Excel.xls", Excel.XlFileFormat.xlWorkbookNormal);
+            LibroExcel.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            //limpiar la y eleminar datos en memoria
+            Marshal.ReleaseComObject(HojaExcel);
+            Marshal.ReleaseComObject(LibroExcel);
+            Marshal.ReleaseComObject(xlApp);
+
+            MessageBox.Show("Proceso creacion excel realizado exitosamente XD!");
+        }
+    }
+}
